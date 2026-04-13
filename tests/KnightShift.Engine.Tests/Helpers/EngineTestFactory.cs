832726f4043b62de0cfd768e173f437f382dbebd
Franz Seckel <fseckel.develop@gmail.com>
2026-04-13 22:27:48 +0200

    test(engine): add EngineTestFactory for reusable test object creation

---
+++ tests/KnightShift.Engine.Tests/Helpers/EngineTestFactory.cs
@@ -0,0 +1,29 @@
+using KnightShift.Engine.Rules;
+using KnightShift.Engine.Moves;
+using KnightShift.Engine.Evaluation;
+using KnightShift.Domain.Core;
+
+namespace KnightShift.Engine.Tests.Helpers;
+
+public static class EngineTestFactory
+{
+    public static MoveValidator CreateMoveValidator()
+    {
+        var checkDetector = new CheckDetector();
+        return new MoveValidator(checkDetector);
+    }
+
+    public static MoveGenerator CreateMoveGenerator()
+    {
+        var moveValidator = CreateMoveValidator();
+        return new MoveGenerator(moveValidator);
+    }
+
+    public static GameResultEvaluator CreateEvaluator()
+    {
+        var checkDetector = new CheckDetector();
+        var moveValidator = new MoveValidator(checkDetector);
+        var moveGenerator = new MoveGenerator(moveValidator);
+        return new GameResultEvaluator(moveGenerator, checkDetector);
+    }
+}
