b9824af3e651bfa7476c7c8cc823b61fbf6814c0
Franz Seckel <fseckel.develop@gmail.com>
2026-04-13 22:02:54 +0200

    test(engine): add MoveGeneratorTests for orchestration and filtering

---
+++ tests/KnightShift.Engine.Tests/Moves/MoveGeneratorTests.cs
@@ -0,0 +1,74 @@
+using FluentAssertions;
+using KnightShift.Domain.Core;
+using KnightShift.Domain.Enums;
+using KnightShift.Engine.Moves;
+using KnightShift.Engine.Rules;
+using KnightShift.Engine.Tests.Helpers;
+
+namespace KnightShift.Engine.Tests.Moves;
+
+public class MoveGeneratorTests
+{
+    private readonly MoveGenerator _generator = new(new MoveValidator(new CheckDetector()));
+
+    [Fact]
+    public void Should_Generate_Moves_Only_For_Current_Player()
+    {
+        var state = new TestGameStateBuilder()
+            .WithPiece(PieceType.King, PieceColor.White, "e1")
+            .WithPiece(PieceType.King, PieceColor.Black, "e8")
+            .WithTurn(PieceColor.White)
+            .Build();
+
+        var moves = _generator.GenerateMoves(state);
+
+        moves.Should().OnlyContain(move => state.Board.GetPiece(move.From)!.Color == PieceColor.White);
+    }
+
+    [Fact]
+    public void Should_Filter_Out_Illegal_Moves()
+    {
+        var state = new TestGameStateBuilder()
+            .WithPiece(PieceType.King, PieceColor.White, "e1")
+            .WithPiece(PieceType.Rook, PieceColor.White, "e2")
+            .WithPiece(PieceType.Rook, PieceColor.Black, "e8")
+            .WithTurn(PieceColor.White)
+            .Build();
+
+        var moves = _generator.GenerateMoves(state);
+
+        moves.Should().NotContain(move =>
+            move.From == Position.CreateFromAlgebraic("e2") &&
+            move.To == Position.CreateFromAlgebraic("f2"));
+    }
+
+    [Fact]
+    public void Should_Generate_Moves_For_All_Pieces()
+    {
+        var state = new TestGameStateBuilder()
+            .WithPiece(PieceType.King, PieceColor.White, "e1")
+            .WithPiece(PieceType.Knight, PieceColor.White, "d4")
+            .WithTurn(PieceColor.White)
+            .Build();
+
+        var moves = _generator.GenerateMoves(state);
+
+        moves.Should().Contain(move => move.From == Position.CreateFromAlgebraic("e1"));
+        moves.Should().Contain(move => move.From == Position.CreateFromAlgebraic("d4"));
+    }
+
+    [Fact]
+    public void Should_Return_Empty_When_No_Legal_Moves()
+    {
+        var state = new TestGameStateBuilder()
+            .WithPiece(PieceType.King, PieceColor.White, "h1")
+            .WithPiece(PieceType.Queen, PieceColor.Black, "g2")
+            .WithPiece(PieceType.Rook, PieceColor.Black, "h2")
+            .WithTurn(PieceColor.White)
+            .Build();
+
+        var moves = _generator.GenerateMoves(state);
+
+        moves.Should().BeEmpty();
+    }
+}
