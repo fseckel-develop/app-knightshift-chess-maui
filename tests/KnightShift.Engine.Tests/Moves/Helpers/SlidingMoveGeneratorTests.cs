ed3319e1b106adb8c1bc27a3833818c4a8f1c0a4
Franz Seckel <fseckel.develop@gmail.com>
2026-04-13 22:01:05 +0200

    test(engine): add SlidingMoveGeneratorTests for directional traversal

---
+++ tests/KnightShift.Engine.Tests/Moves/Helpers/SlidingMoveGeneratorTests.cs
@@ -0,0 +1,111 @@
+using FluentAssertions;
+using KnightShift.Domain.Core;
+using KnightShift.Domain.Enums;
+using KnightShift.Engine.Moves.Helpers;
+using KnightShift.Engine.Tests.Helpers;
+
+namespace KnightShift.Engine.Tests.Moves.Helpers;
+
+public class SlidingMoveGeneratorTests
+{
+    [Fact]
+    public void Should_Generate_Moves_Along_Direction_Until_Boundary()
+    {
+        var state = new TestGameStateBuilder()
+            .WithPiece(PieceType.Rook, PieceColor.White, "d4")
+            .Build();
+
+        var directions = new[] { (-1, 0) };
+
+        var moves = SlidingMoveGenerator.GenerateSlidingMoves(
+            state,
+            new Piece(PieceType.Rook, PieceColor.White),
+            Position.CreateFromAlgebraic("d4"),
+            directions);
+
+        moves.Should().Contain(move => move.To == Position.CreateFromAlgebraic("d5"));
+        moves.Should().Contain(move => move.To == Position.CreateFromAlgebraic("d8"));
+    }
+
+    [Fact]
+    public void Should_Stop_At_Own_Piece_Without_Capturing()
+    {
+        var state = new TestGameStateBuilder()
+            .WithPiece(PieceType.Rook, PieceColor.White, "d4")
+            .WithPiece(PieceType.Pawn, PieceColor.White, "d6")
+            .Build();
+
+        var directions = new[] { (-1, 0) };
+
+        var moves = SlidingMoveGenerator.GenerateSlidingMoves(
+            state,
+            new Piece(PieceType.Rook, PieceColor.White),
+            Position.CreateFromAlgebraic("d4"),
+            directions);
+
+        moves.Should().NotContain(move => move.To == Position.CreateFromAlgebraic("d6"));
+        moves.Should().NotContain(move => move.To == Position.CreateFromAlgebraic("d7"));
+    }
+
+    [Fact]
+    public void Should_Capture_Enemy_Piece_And_Stop()
+    {
+        var state = new TestGameStateBuilder()
+            .WithPiece(PieceType.Rook, PieceColor.White, "d4")
+            .WithPiece(PieceType.Pawn, PieceColor.Black, "d6")
+            .Build();
+
+        var directions = new[] { (-1, 0) };
+
+        var moves = SlidingMoveGenerator.GenerateSlidingMoves(
+            state,
+            new Piece(PieceType.Rook, PieceColor.White),
+            Position.CreateFromAlgebraic("d4"),
+            directions);
+
+        moves.Should().Contain(move => move.To == Position.CreateFromAlgebraic("d6"));
+        moves.Should().NotContain(move => move.To == Position.CreateFromAlgebraic("d7"));
+    }
+
+    [Fact]
+    public void Should_Stop_At_Board_Boundary()
+    {
+        var state = new TestGameStateBuilder()
+            .WithPiece(PieceType.Rook, PieceColor.White, "d8")
+            .Build();
+
+        var directions = new[] { (-1, 0) };
+
+        var moves = SlidingMoveGenerator.GenerateSlidingMoves(
+            state,
+            new Piece(PieceType.Rook, PieceColor.White),
+            Position.CreateFromAlgebraic("d8"),
+            directions);
+
+        moves.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void Should_Generate_Moves_In_Multiple_Directions()
+    {
+        var state = new TestGameStateBuilder()
+            .WithPiece(PieceType.Rook, PieceColor.White, "d4")
+            .Build();
+
+        var directions = new[]
+        {
+            (1, 0), (-1, 0), (0, 1), (0, -1)
+        };
+
+        var moves = SlidingMoveGenerator.GenerateSlidingMoves(
+            state,
+            new Piece(PieceType.Rook, PieceColor.White),
+            Position.CreateFromAlgebraic("d4"),
+            directions);
+
+        moves.Should().Contain(moves => moves.To == Position.CreateFromAlgebraic("d8"));
+        moves.Should().Contain(moves => moves.To == Position.CreateFromAlgebraic("d1"));
+        moves.Should().Contain(moves => moves.To == Position.CreateFromAlgebraic("a4"));
+        moves.Should().Contain(moves => moves.To == Position.CreateFromAlgebraic("h4"));
+    }
+}
