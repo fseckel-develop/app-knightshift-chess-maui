53a5ef1efb461a8819c50c218045855482ff73e6
Franz Seckel <fseckel.develop@gmail.com>
2026-04-15 19:48:19 +0200

    feat(application): add mappers for domain-to-dto conversion

---
+++ src/KnightShift.Application/Mappers/MoveMapper.cs
@@ -0,0 +1,31 @@
+using KnightShift.Application.DTOs;
+using KnightShift.Domain.Core;
+using KnightShift.Domain.Enums;
+
+namespace KnightShift.Application.Mappers;
+
+public static class MoveMapper
+{
+    public static MoveDto ToDto(Move move)
+    {
+        return new MoveDto
+        {
+            Origin = move.Origin.ToString(),
+            Target = move.Target.ToString(),
+            Promotion = move.Promotion?.ToString(),
+            IsCastling = move.IsCastling,
+            IsEnPassant = move.IsEnPassant
+        };
+    }
+
+    public static Move FromDto(MoveDto dto)
+    {
+        return new Move(
+            Position.CreateFromAlgebraic(dto.Origin),
+            Position.CreateFromAlgebraic(dto.Target),
+            dto.Promotion is null ? null : Enum.Parse<PieceType>(dto.Promotion),
+            dto.IsCastling,
+            dto.IsEnPassant
+        );
+    }
+}
