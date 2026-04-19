375087ced5514d54d39b7d6270404ed36ac92d52
Franz Seckel <fseckel.develop@gmail.com>
2026-04-20 01:15:04 +0200

    feat(application): extend DTOs for UI rendering

--- src/KnightShift.Application/Contracts/DTOs/GameStateDto.cs
+++ src/KnightShift.Application/Contracts/DTOs/GameStateDto.cs
@@ -1,10 +1,14 @@
+using KnightShift.Domain.Constants;
+
 namespace KnightShift.Application.Contracts.DTOs;
 
 public class GameStateDto
 {
-    public string[][] Board { get; set; } = default!;
+    public PieceDto?[,] Board { get; set; } = new PieceDto[BoardDimensions.Size, BoardDimensions.Size];
 
     public string CurrentTurn { get; set; } = default!;
+
+    public MoveDto? LastMove { get; set; }
 
     public string Result { get; set; } = default!;
 
--- src/KnightShift.Application/Contracts/DTOs/MoveDto.cs
+++ src/KnightShift.Application/Contracts/DTOs/MoveDto.cs
@@ -5,6 +5,12 @@ public class MoveDto
     public string Origin { get; set; } = default!;
     public string Target { get; set; } = default!;
 
+    public int OriginRow { get; set; }
+    public int OriginColumn { get; set; }
+
+    public int TargetRow { get; set; }
+    public int TargetColumn { get; set; }
+
     public string? Promotion { get; set; }
 
     public bool IsCastling { get; set; }
