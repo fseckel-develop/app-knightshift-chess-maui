18b74137743611822776aef8359c51306eb0f590
Franz Seckel <fseckel.develop@gmail.com>
2026-05-09 16:12:55 +0200

    chore: initilize MAUI project

---
+++ src/KnightShift.Maui/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace KnightShift.Maui;
+
+public partial class App : Microsoft.Maui.Controls.Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
